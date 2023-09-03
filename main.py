361ece14c9f8f384a6b901c155c80d599b86b4d1
ibrahimiqra <ibrahimiqra1@gmail.com>
2023-09-03 17:02:29 +0600

    added print statements for further clarifications

--- main.py
+++ main.py
@@ -7,20 +7,26 @@ from dataset import prepare_dataset
 def main(cfg: DictConfig):
 
     #1. Parse config and get experiment output dir
+    print("=====Config:=====\n")
     print(OmegaConf.to_yaml(cfg))
+    print("=================\n\n")
+
 
     #2. Preparing Dataset
     train_loaders, \
-        validation_loaders, \
+        val_loaders, \
             test_loader \
                 = prepare_dataset(
                     num_partition=cfg.num_clients,
                     batch_size=cfg.batch_size
                 )
 
-    print( f"{len(train_loaders)=}\n{len(validation_loaders)=}\n{len(test_loader)=}" )
-    print('----------')
-    print( f"{len(train_loaders[0])=}\n{len(validation_loaders[0])=}\n{len(test_loader)=}" )
+    print('-------Loaders Information-------')
+    print( f"Total Train_Loaders(for each clinents): {len(train_loaders)}, Each Train_Loader has {len(train_loaders[0].dataset)} data\
+          \nTotal Val_Loaders(for each clinents): {len(val_loaders)}, Each Val_Loader has {len(val_loaders[0].dataset)} data{len(val_loaders)=}\
+          \nTotal Test_Loaders {len(test_loader)=}")
+    print('---------------------------------\n\n')
+
     #3. Defining clients
 
     #4. Define the strategy
